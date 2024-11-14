2d24ce82864c85ed45eeb7ce34ff0a271db86264
Angell Pimienta's <angelefrain1510@gmail.com>
2024-11-14 14:53:52 -0500

    media_muestral: Desviacion estandar

--- Almanaque/almanaque.py
+++ Almanaque/almanaque.py
@@ -16,56 +16,97 @@ def demanda():
     vendido = 300
   return vendido
 
-# Valor esperado / varianza (variable discreta)
 almanaqueVendido = [100,150,200,250,300]
 probabilidadVenta = [0.3,0.2,0.3,0.15,0.05]
-def valorEsperado():
-  valor = 0
-  for i in range(5):
-    valor += (almanaqueVendido[i]*probabilidadVenta[i])
-  return valor
-# Varianza = E[x**2] - (E[x])**2
 
-def main():
+
+def calcularN(sigma):
+  n = ((1.96**2) * (sigma ** 2) ) / 100
+  return n
+
+
+def main(n):
   simulado = demanda()
-  # x = int(input("Ingrese el numero de almanaques a comprar: "))
-  cantidad_importar = 100
+  cantidad_importar = n
+  precio_simulado = (0.5 * r.random())+1.5
   while simulado > cantidad_importar:
     simulado = demanda()
-  precio_simulado = (0.5 * r.random())+1.5
   no_vendidos = cantidad_importar - simulado
-  utilidad_unitaria = 4.5 - precio_simulado
 
-  utilidad_maxima = utilidad_unitaria * cantidad_importar + no_vendidos * 0.75
+  utilidad_maxima = (4.5 * simulado + no_vendidos * 0.75)-precio_simulado * cantidad_importar
 
   return utilidad_maxima
 
 
-utilidad_all = []
+def main_secction(n):
+  utilidad = []
+  for i in range(10000):
+    ganancia = main(n)
+    utilidad.append(ganancia)
+  desviacion_muestral = np.std(utilidad)
+  print(f"Desviacion estandar con un importe {n} en muestra de 10000: {desviacion_muestral}")
+  n = calcularN(desviacion_muestral)
+  return int(n)
 
-for i in almanaqueVendido:
-  print(i)
-  lista_utilidades = []
-  for j in range(10000):
-      m = main()
-      lista_utilidades.append(m)
-  media = np.std(lista_utilidades)
-  utilidad_all.append(media)
 
-# for i in range(10000):
+def master():
+  # ideal = [main_secction(100), main_secction(150), main_secction(200), main_secction(250), main_secction(300) ]
+  for i in almanaqueVendido:
+    ideal = main_secction(i)
+    utilidad_ideal = []
+    for j in range(ideal):
+      utilidad_ideal.append(main(i))
+    desviacion_ideal = np.std(utilidad_ideal)
+    media_ideal_ganancias = np.mean(utilidad_ideal)
+    print(f"Desviacion_ideal: {desviacion_ideal} \nMedia ideal (X): {media_ideal_ganancias}\n")
+    utilidad_ideal = []
 
-for i in utilidad_all:
-  print(i)
 
-# desviacion_estandar = np.std(utilidad_100)
+
+master()
+
+# iteracion_ideal = main_secction(250)
+
+# print(iteracion_ideal)
+# utilidad_ideal = []
+# for i in range(int(iteracion_ideal)):
+#   utilidad_ideal.append(main(250))
+
+# print(np.std(utilidad_ideal))
+
+
+
+
+
+
+
+
+
+# utilidad_all = []
+
+# for i in almanaqueVendido:
+#   lista_utilidades = []
+#   for j in range(10000):
+#       m = main(i)
+#       lista_utilidades.append(m)
+
+#   sigma = np.std(lista_utilidades)
+#   utilidad_all.append(sigma)
+
+
+# for i in utilidad_all:
+#   print(calcularN(i))
+
+
+
+
+
+
+# desviacion_estandar = np.std(utilidad_all)
 # intervalo_confianza = (1.96 * desviacion_estandar)/np.sqrt(10000)
 # print('Desviación estandar', desviacion_estandar)
 # print('Media:', media)
 
 
 # print(f"Intervalo de confianza {media} +/- {intervalo_confianza}")
-
-
-
-# n = ((1.96**2) * (desviacion_estandar ** 2) ) / 100
 # print('Ensayos requeridos', n)
