8bf054353daa701538b57cc8d6f6c9e8e3018b88
Angell Pimienta's <angelefrain1510@gmail.com>
2024-11-29 10:41:01 -0500

    task 1 for montecarlo simulation

--- Generacion VA/generacion_1.py
+++ Generacion VA/generacion_1.py
@@ -15,3 +15,5 @@ for i in range(ite):
 mayor_3_4 = sum([1 for c in circunferencias if c > 3.4])/ite    # 4% aprox
 
 desecho = sum([1 for c in circunferencias if c > 3.5 or c < 3.3])/ite    # 90% apro
+
+print(mayor_3_4, desecho)
