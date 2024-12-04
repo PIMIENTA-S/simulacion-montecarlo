f6b62e26c958fb8566c2fbebb23cd35fe7b01187
Angell Pimienta's <angelefrain1510@gmail.com>
2024-12-03 22:19:24 -0500

    sucss file for generation_entregable

---
+++ Generacion VA/Entregable_GeneracionVA/generacion_1.py
@@ -0,0 +1,19 @@
+import random
+import math
+
+def circunferencia(alfa, beta, v):
+  r = random.random()
+  return (alfa*(-math.log(1-r))**(1/beta))+v
+
+circunferencias = []
+
+ite = 10000
+
+for i in range(ite):
+  circunferencias.append(circunferencia(0.005, 1/3, 3.25))
+
+mayor_3_4 = sum([1 for c in circunferencias if c > 3.4])/ite    # 4% aprox
+
+desecho = sum([1 for c in circunferencias if c > 3.5 or c < 3.3])/ite    # 90% apro
+
+print(mayor_3_4*100, desecho*100)
